bff5be90540fc3b656a1279dda76c32ef18ef342
Vasily Isaev <vasyaisaev31@gmail.com>
2018-11-01 02:50:51 +0300

    added 7_1 and 7_2 lab's

--- Task_6.java
+++ Task_6.java
@@ -1,4 +1,4 @@
-
+/*
 public class MyList<T> {
 	
 	Node<T> first;
@@ -26,4 +26,25 @@ public class MyList<T> {
 		return s + "]";
 	}
 
+}*/
+class Task_6 {
+	public static boolean isSubstring(String origin, String sub) {
+		for (int i = 0; i <= origin.length() - sub.length(); i++) {
+			if (sub.charAt(0) == origin.charAt(i)) {
+				for (int j = i; j < i + sub.length(); j++) {
+					if (sub.charAt(j - i) != origin.charAt(j ))
+						break;
+					if (j == i + sub.length() - 1)
+						return true;
+				}
+
+			}
+		}
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isSubstring("abcdefg" , "ce"));
+	}
 }
