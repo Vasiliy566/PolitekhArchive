e1f34a61837aa256304e2fe18f2c1d8438ddd0c3
Vasily Isaev <vasyaisaev31@gmail.com>
2018-11-01 01:32:23 +0300

    changed files names

---
+++ Task_6.java
@@ -0,0 +1,29 @@
+
+public class MyList<T> {
+	
+	Node<T> first;
+	
+	public void add(T data) {
+		Node<T> node = new Node<T>();
+		node.data = data;
+		node.next = first;
+		first = node;
+	}
+	
+	@Override
+	public String toString() {
+		
+		String s = "[";
+		
+		Node temp = first;
+		
+		while (temp != null) {
+			s += temp.data;
+			s += ", ";
+			temp = temp.next;
+		}
+		
+		return s + "]";
+	}
+
+}
