abc966f428062a1f79599ece9106f6ffa6676fd2
Vasily Isaev <vasyaisaev31@gmail.com>
2018-11-01 03:08:48 +0300

    added 8 and 9 lab's

---
+++ Task_9.java
@@ -0,0 +1,35 @@
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Task_9 {
+	// Path to .properties file
+	public static final String PATH_TO_PROPERTIES = "src/main/resources/config.properties";
+
+	public static String TakeInfo() {
+
+		FileInputStream fileInputStream;
+		Properties prop = new Properties();
+
+		try {
+			// try to take info from fole
+			fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
+			prop.load(fileInputStream);
+
+			String site = prop.getProperty("site");
+			String loginToSite = prop.getProperty("login");
+			String passwordToSite = prop.getProperty("password");
+
+			// return taken data
+			return ("site: " + site + "\nloginToSite: " + loginToSite + "\npasswordToSite: " + passwordToSite);
+
+		} catch (IOException e) {
+			System.out.println("Error: file " + PATH_TO_PROPERTIES + " not exist");
+			e.printStackTrace();
+			return "error";
+		}
+
+	}
+
+}
